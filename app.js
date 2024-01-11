c4dceb6571eb71e39223b4849867d65ebd5cf7b2
Yuga Handika <66674783+hanhanyugs@users.noreply.github.com>
2024-01-11 18:17:02 +0700

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
   var qrCodeContainer = document.getElementById('qrcodes');
 
-  var urlParams = new URLSearchParams(window.location.search);
-  var guestName = urlParams.get('nama');
-  var seatNumber = urlParams.get('seating');
+  // Ganti URL berikut sesuai dengan URL yang dihasilkan oleh skrip Apps Script di Google Sheets
+  var urlFromSheet = "https://hanhanyugs.github.io/PolibeliQR/?nama=John&seating=0.02";
 
-  if (guestName && seatNumber) {
-    var infoText = document.createElement('p');
-    infoText.textContent = "Hello " + guestName + ", your seating location is at " + seatNumber + ".";
-    qrCodeContainer.appendChild(infoText);
+  // Fungsi untuk menambahkan QR Code ke elemen dengan id 'qrcodes'
+  function addQRCode(url) {
+    var qrCode = new QRCode(qrCodeContainer, {
+      text: url,
+      width: 150,
+      height: 150
+    });
+  }
+
+  // Fungsi untuk mengecek keberadaan parameter yang diperlukan
+  function isValidURL(url) {
+    var urlParams = new URLSearchParams(url);
+    var guestName = urlParams.get('nama');
+    var seatNumber = urlParams.get('seating');
+    return guestName && seatNumber;
+  }
+
+  // Panggil fungsi addQRCode dengan URL yang sesuai
+  if (isValidURL(urlFromSheet)) {
+    addQRCode(urlFromSheet);
   } else {
     var errorText = document.createElement('p');
-    errorText.textContent = "Invalid URL or missing information.";
+    errorText.textContent = "Invalid QR Code or missing information.";
     qrCodeContainer.appendChild(errorText);
   }
 });

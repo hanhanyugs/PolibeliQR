41b6f185bb6df98bdc32f67ec3318d467bb18e2a
Yuga Handika <66674783+hanhanyugs@users.noreply.github.com>
2024-01-11 23:17:13 +0700

    app.js

--- app.js
+++ app.js
@@ -9,21 +9,46 @@ document.addEventListener('DOMContentLoaded', function () {
   var seatingNumberElements = document.querySelectorAll('.seating-number');
   var customerNameElements = document.querySelectorAll('.customer-name');
 
-  // Loop melalui elemen-elemen dan buat QR Code
-  for (var i = 0; i < seatingNumberElements.length; i++) {
-    var seatingNumber = seatingNumberElements[i].textContent.trim();
-    var customerName = customerNameElements[i].textContent.trim();
+  // Logika polling untuk mendapatkan data terbaru
+  function pollLatestData() {
+    fetch('https://script.google.com/macros/s/AKfycbwPJtqFPLRCWLaQiTEz_9cQTZSYJvDK1WO7hVt79ss/exec/getLatestData')
+      .then(response => response.json())
+      .then(data => {
+        // Lakukan sesuatu dengan data yang diterima
+        console.log('Received latest data:', data);
 
-    // Jika ada informasi nama dan nomor seating, buat URL GitHub Pages
-    if (customerName !== "" && seatingNumber !== "") {
-      var githubPagesUrl = githubPagesBaseUrl + "?nama=" + encodeURIComponent(customerName) + "&seating=" + encodeURIComponent(seatingNumber);
+        // Perbarui tampilan berdasarkan data terbaru
+        updateView(data);
+      })
+      .catch(error => console.error('Error:', error));
+  }
 
-      // Membuat QR Code
-      var qrCode = new QRCode(qrCodeContainer, {
-        text: githubPagesUrl,
-        width: 150,
-        height: 150
-      });
+  // Fungsi untuk memperbarui tampilan berdasarkan data terbaru
+  function updateView(data) {
+    // Loop melalui elemen-elemen dan buat QR Code
+    for (var i = 0; i < seatingNumberElements.length; i++) {
+      var seatingNumber = seatingNumberElements[i].textContent.trim();
+      var customerName = customerNameElements[i].textContent.trim();
+
+      // Jika ada informasi nama dan nomor seating, buat URL GitHub Pages
+      if (customerName !== "" && seatingNumber !== "") {
+        var githubPagesUrl = githubPagesBaseUrl + "?nama=" + encodeURIComponent(customerName) + "&seating=" + encodeURIComponent(seatingNumber);
+
+        // Membuat QR Code
+        var qrCode = new QRCode(qrCodeContainer, {
+          text: githubPagesUrl,
+          width: 150,
+          height: 150
+        });
+      }
     }
   }
+
+  // Panggil fungsi polling saat halaman dimuat
+  pollLatestData();
+
+  // Atur interval untuk polling setiap 5 detik
+  setInterval(function () {
+    pollLatestData();
+  }, 5000);
 });

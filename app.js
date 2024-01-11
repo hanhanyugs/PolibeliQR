746bd4608f3a362269c2fe6cc99e148240999ac5
Yuga Handika <66674783+hanhanyugs@users.noreply.github.com>
2024-01-11 18:34:39 +0700

    app.js

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Ganti 'USERNAME' dan 'REPO' sesuai dengan informasi GitHub Anda
+  var githubPagesBaseUrl = "https://hanhanyugs.github.io/PolibeliQR/";
+
+  // Ambil elemen container untuk menampilkan QR Code
   var qrCodeContainer = document.getElementById('qrcodes');
 
-  // Ganti URL berikut sesuai dengan URL yang dihasilkan oleh skrip Apps Script di Google Sheets
-  var urlFromSheet = "https://hanhanyugs.github.io/PolibeliQR/?nama=John&seating=0.02";
+  // Ambil data dari kolom B (nomor seating) dan C (nama customer)
+  var seatingNumberElements = document.querySelectorAll('.seating-number');
+  var customerNameElements = document.querySelectorAll('.customer-name');
 
-  // Fungsi untuk mendapatkan nilai parameter dari URL
-  function getParameterValue(url, parameterName) {
-    var urlParams = new URLSearchParams(url);
-    return urlParams.get(parameterName);
-  }
+  // Loop melalui elemen-elemen dan buat QR Code
+  for (var i = 0; i < seatingNumberElements.length; i++) {
+    var seatingNumber = seatingNumberElements[i].textContent.trim();
+    var customerName = customerNameElements[i].textContent.trim();
 
-  // Mendapatkan nilai parameter 'nama' dan 'seating' dari URL
-  var guestName = getParameterValue(urlFromSheet, 'nama');
-  var seatNumber = getParameterValue(urlFromSheet, 'seating');
+    // Jika ada informasi nama dan nomor seating, buat URL GitHub Pages
+    if (customerName !== "" && seatingNumber !== "") {
+      var githubPagesUrl = githubPagesBaseUrl + "?nama=" + encodeURIComponent(customerName) + "&seating=" + encodeURIComponent(seatingNumber);
 
-  // Fungsi untuk menambahkan QR Code ke elemen dengan id 'qrcodes'
-  function addQRCode(url) {
-    var qrCode = new QRCode(qrCodeContainer, {
-      text: url,
-      width: 150,
-      height: 150
-    });
-  }
-
-  // Memeriksa keberadaan parameter yang diperlukan
-  if (guestName && seatNumber) {
-    // Jika parameter ada, maka tampilkan informasi
-    var infoText = document.createElement('p');
-    infoText.textContent = "Hello " + guestName + ", your seating location is at " + seatNumber + ".";
-    qrCodeContainer.appendChild(infoText);
-  } else {
-    // Jika parameter tidak lengkap, tampilkan pesan kesalahan
-    var errorText = document.createElement('p');
-    errorText.textContent = "Invalid QR Code or missing information.";
-    qrCodeContainer.appendChild(errorText);
+      // Membuat QR Code
+      var qrCode = new QRCode(qrCodeContainer, {
+        text: githubPagesUrl,
+        width: 150,
+        height: 150
+      });
+    }
   }
 });

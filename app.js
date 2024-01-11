32b52ae002a4d69085f18b8ec25e9910da635db4
Yuga Handika <66674783+hanhanyugs@users.noreply.github.com>
2024-01-11 18:09:09 +0700

    app.js

--- app.js
+++ app.js
@@ -2,23 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   var qrCodeContainer = document.getElementById('qrcodes');
 
   var urlParams = new URLSearchParams(window.location.search);
-  var code = urlParams.get('code');
-  var seatNumber = urlParams.get('seat');
-  var guestName = urlParams.get('name');
+  var guestName = urlParams.get('nama');
+  var seatNumber = urlParams.get('seating');
 
-  if (code && seatNumber && guestName) {
-    var qrCode = new QRCode(qrCodeContainer, {
-      text: "Code: " + code + "\nSeat: " + seatNumber,
-      width: 150,
-      height: 150
-    });
-
+  if (guestName && seatNumber) {
     var infoText = document.createElement('p');
     infoText.textContent = "Hello " + guestName + ", your seating location is at " + seatNumber + ".";
     qrCodeContainer.appendChild(infoText);
   } else {
     var errorText = document.createElement('p');
-    errorText.textContent = "Invalid QR Code or missing information.";
+    errorText.textContent = "Invalid URL or missing information.";
     qrCodeContainer.appendChild(errorText);
   }
 });
